64b92569606f5338346ca691d3d7925559612bfd
william <wgoku885@gmail.com>
2023-06-05 04:53:57 -0700

    added array.php

---
+++ array.php
@@ -0,0 +1,6 @@
+<?php
+echo "this is the lecture on arrays in php  ";
+
+$articles = ['this is the first one', 'this is the second one', "this is the third one"];
+
+var_dump($articles[2]);
---
+++ math.php
@@ -0,0 +1,13 @@
+<?php
+
+$price = 4.99;
+$quantity = 4;
+var_dump($price + $quantity);
+
+//operation of string 
+//the dot operator (.) is used for concatination of two or more string data items. 
+// example
+
+$message = "hello";
+$name = "Dave";
+echo $message . " ".$name;
